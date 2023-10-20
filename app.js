67a27905aa41e1de6d310f623d30f2b71055b15e
sainath-WHIP <tech@whiprides.org>
2023-10-20 17:02:11 +0530

    tretr

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const path = require("path");
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://admin.serrano.in");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", "true"); // Set the header to true
   next();
 });
 // app.use(cors({
