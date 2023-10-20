ad0bb686749babaff8460e0dab9a3156746091c2
sainath-WHIP <tech@whiprides.org>
2023-10-20 17:04:18 +0530

    ytrty

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require("path");
 
 // Allow requests from specific origins
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://admin.serrano.in");
+  res.header("Access-Control-Allow-Origin", "https://admin.serrano.in", "https://shop.serrano.in");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", "true"); // Set the header to true
   next();
